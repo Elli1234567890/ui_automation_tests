2d4a9a3dbbdd7cd4c365554408a7c1090471f0a9
Tatyna <rinatiadzua@gmail.com>
2026-04-18 17:17:34 +0300

    feat: Создание UI automation tests с Allure, параллельным запуском и CI/CD

--- utils/helpers.py
+++ utils/helpers.py
@@ -3,7 +3,6 @@ import allure
 
 
 def attach_screenshot(driver, name="screenshot"):
-    """Добавить скриншот в отчет Allure"""
     allure.attach(
         driver.get_screenshot_as_png(),
         name=name,
@@ -12,5 +11,4 @@ def attach_screenshot(driver, name="screenshot"):
 
 
 def get_random_quantity(min_qty=1, max_qty=5):
-    """Получить случайное количество"""
     return random.randint(min_qty, max_qty)
